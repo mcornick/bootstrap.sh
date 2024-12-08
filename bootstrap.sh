d53eff05c803f2fb86c6669e6e42e68f0e56e937
Mark Cornick <mcornick@mcornick.com>
2024-12-07 19:33:11 -0500

    Eliminate PS1 repetition by stubbing __git_ps1

--- bootstrap.sh
+++ bootstrap.sh
@@ -44,16 +44,16 @@ echo "set completion-ignore-case On" >"$HOME/.inputrc"
 cat <<E0F >"$HOME/.bashrc"
 if [ -f "\$HOME/.git-prompt.sh" ]; then
   . "\$HOME/.git-prompt.sh"
-  PS1='\u@\h:\w\$(__git_ps1 " (%s)")\\$ '
 elif [ -f "$prefix/share/git-core/contrib/completion/git-prompt.sh" ]; then
   . "$prefix/share/git-core/contrib/completion/git-prompt.sh"
-  PS1='\u@\h:\w\$(__git_ps1 " (%s)")\\$ '
 elif [ -f "$prefix/lib/git-core/git-sh-prompt" ]; then
   . "$prefix/lib/git-core/git-sh-prompt"
-  PS1='\u@\h:\w\$(__git_ps1 " (%s)")\\$ '
 else
-  PS1="\u@\h:\w\$ "
+  __git_ps1() {
+    true
+  }
 fi
+PS1='\u@\h:\w\$(__git_ps1 " (%s)")\\$ '
 E0F
 ##### Use color ls on Linux, standard ls elsewhere
 if [ "$(uname)" = "Linux" ]; then
